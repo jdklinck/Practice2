d507b1a0727919f05da7f77e4adebb5678d15b24
unknown <jessicaklinck@yahoo.com>
2020-11-18 11:05:55 -0500

    Prac2

---
+++ PracticeGit2/Model/Dog.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeGit2.Model
+{
+    public class Dog
+    {
+        public string Lab { get; set; }
+    }
+}
